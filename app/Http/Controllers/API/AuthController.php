a76ca0bdaa087274aca2d0ea0858f06b9f591ce9
Ghivarra Senandika Rushdie <gsenandika@gmail.com>
2025-07-11 17:48:14 +0700

    done auth controller register,login,logour

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rules\Password;
 
 class AuthController extends Controller
 {
@@ -47,7 +48,7 @@ class AuthController extends Controller
 
         // get token
         // with one day expiration
-        $accessToken  = $user->createToken('acess-token', ['role' => 1], Carbon::now()->addDay(1))->plainTextToken;
+        $accessToken  = $user->createToken('acess-token', Carbon::now()->addDay(1))->plainTextToken;
 
         // return response
         return response()->json([
@@ -56,6 +57,70 @@ class AuthController extends Controller
             'data'    => [
                 'access_token' => $accessToken,
             ]
+        ], 200);
+    }
+
+    //===========================================================================================
+
+    public function logout(Request $request)
+    {
+        // revoke token
+        $request->user()->currentAccessToken()->delete();
+
+        // return ok
+        return response()->json([
+            'status'  => 'success',
+            'message' => 'Berhasil keluar, token anda sudah dihapus',
+        ], 200);
+    }
+
+    //===========================================================================================
+
+    public function register(Request $request): JsonResponse
+    {
+        $validation = Validator::make($request->all(), [
+            'name'      => ['required', 'max:200'],
+            'email'     => ['required', 'email', 'unique:users,email', 'max:200'],
+            'password'  => ['required', 'confirmed', Password::min(10)->letters()->mixedCase()->numbers()->symbols()],
+        ]);
+
+        // check if error
+        if ($validation->fails())
+        {
+            // return error response
+            return response()->json([
+                'status'  => 'error',
+                'message' => 'Akun email dan password tidak cocok',
+                'data'    => [
+                    'errors' => $validation->errors()
+                ]
+            ], 422);
+        }
+
+        // get data
+        $input = $validation->validated();
+
+        // input user
+        $user = new User;
+        $user->name = $input['name'];
+        $user->email = $input['email'];
+        $user->password = $input['password'];
+        $isSuccess = $user->save();
+
+        if (!$isSuccess)
+        {
+            // return error response
+            return response()->json([
+                'status'  => 'error',
+                'message' => 'Server sedang sibuk, silahkan coba lagi',
+            ], 503);
+        }
+
+        // return
+        return response()->json([
+            'status'  => 'success',
+            'message' => "User {$user->name} berhasil ditambahkan",
+            'data'    => $user
         ], 200);
     }
 
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Field;
+use App\Models\Subfield;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,12 +15,35 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            
-        ]);
+        $this->fieldSeeder();
+        $this->subfieldSeeder();
     }
+
+    //=================================================================================================
+
+    private function fieldSeeder(): void
+    {
+        $fields = [
+            ['name' => 'Konstruksi - Jasa Pelaksana'], // id = 1
+            ['name' => 'Sistem Informasi - Jasa Pelaksana'], // id = 2
+        ];
+
+        Field::insert($fields);
+    }
+
+    //=================================================================================================
+
+    private function subfieldSeeder(): void
+    {
+        $subfields = [
+            ['field_id' => 1, 'name' => 'Pelaksana konstruksi jalan raya dan rel kereta api'],
+            ['field_id' => 1, 'name' => 'Pelaksana konstruksi bangunan jenis rumah hunian'],
+            ['field_id' => 2, 'name' => 'Pelaksana pembuatan jaringan komunikasi dan internet bangunan'],
+            ['field_id' => 2, 'name' => 'Pelaksana pembuatan dan pengembangan aplikasi'],
+        ];
+
+        Subfield::insert($subfields);
+    }
+
+    //=================================================================================================
 }
