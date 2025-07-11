55c43d1e70f108ae396ecdf4d5f81cfb33fbb6c6
Ghivarra Senandika Rushdie <gsenandika@gmail.com>
2025-07-11 17:08:33 +0700

    fix wrong table name

--- app/Models/Vendors.php
+++ app/Models/Vendors.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Field extends Model
+class Vendor extends Model
 {
     use SoftDeletes;
 
@@ -14,7 +14,7 @@ class Field extends Model
      *
      * @var string
      */
-    protected $table = 'fields';
+    protected $table = 'vendors';
 
      /**
      * The primary key associated with the table.
